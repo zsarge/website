3d1e3c74a9f3e07495abd4f8d60264581b43b2f6
Zack Sargent <zackarywsargent@gmail.com>
2022-11-02 12:05:14 -0400

    apply basic rules for conway's game of life

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ class Board {
    * @returns {number} (Uint8)
    */
   get(x, y) {
+    // wrap board like a tourus
+    if (y < 0) y = this.height + y;
+    if (x < 0) x = this.width + x;
+    if (y >= this.height) y = this.height - y;
+    if (x >= this.width) x = this.width - x;
+
     return this.board[y * this.width + x];
   }
 
@@ -48,7 +54,7 @@ class GameOfLife {
     /** @type CanvasRenderingContext2D */
     this.ctx = canvas.getContext("2d");
 
-    const SQUARE_SIZE = 5; // size of cell in pixels
+    const SQUARE_SIZE = 10; // size of cell in pixels
     /** @const {number} */
     this.SQUARE_SIZE = SQUARE_SIZE;
 
@@ -59,6 +65,8 @@ class GameOfLife {
 
     /** @type Board */
     this.board = new Board(this.cellsWide, this.cellsHigh);
+    /** @type Board */
+    this.nextBoard = new Board(this.cellsWide, this.cellsHigh);
   }
 
   /**
@@ -85,6 +93,91 @@ class GameOfLife {
       }
     }
   }
+
+  /**
+   * Counts the neighbors at a location
+   * @param {number} x
+   * @param {number} y
+   * @returns {number}
+   */
+  #countNeighbors(x, y) {
+    let n = 0;
+    // dx and dy are deltas from the given (x, y) point,
+    // Here is a graph:
+    /*
+     * (x-1, y-1) | (x, y-1) | (x+1, y-1)
+     * (x-1, y  ) | (x, y  ) | (x+1, y  )
+     * (x-1, y+1) | (x, y+1) | (x+1, y+1)
+     */
+    for (let dx = -1; dx <= 1; dx++)
+      for (let dy = -1; dy <= 1; dy++)
+        // make sure we don't count the square we're checking around
+        if (x + dx != x || y + dy != y)
+          if (this.board.get(x + dx, y + dy) == 1)
+            // count alive squares
+            n++;
+    return n;
+  }
+
+  /**
+   * Sets the value of the next board, as to not
+   * interfere with the logic of the current pass.
+   * @param {number} x
+   * @param {number} y
+   * @param {number} value
+   */
+  #setNext(x, y, value) {
+    this.nextBoard.set(x, y, value);
+  }
+
+  /**
+   * Applies the rules of Conway's game of life
+   * @param {number} x
+   * @param {number} y
+   */
+  #applyRules(x, y) {
+    let n = this.#countNeighbors(x, y);
+
+    // Any live cell
+    if (this.board.get(x, y) == 1) {
+      if (n < 2)
+        // with fewer than two live neighbours
+        // dies, as if by underpopulation.
+        this.#setNext(x, y, 0);
+      else if (n > 3)
+        // with more than three live neighbours
+        // dies, as if by overpopulation.
+        this.#setNext(x, y, 0);
+      // with two or three live neighbours
+      // lives on to the next generation.
+      else this.#setNext(x, y, 1);
+    } else {
+      // Any dead cell with exactly three live neighbours
+      // becomes a live cell, as if by reproduction.
+      if (n == 3) this.#setNext(x, y, 1);
+      else this.#setNext(x, y, 0);
+    }
+  }
+  /**
+   * Updates the game state
+   */
+  tick() {
+    // generate next frame
+    for (let y = 0; y < this.cellsHigh; y++) {
+      for (let x = 0; x < this.cellsWide; x++) {
+        this.#applyRules(x, y);
+      }
+    }
+  }
+
+  /**
+   * Swaps the two boards
+   */
+  swapBuffers() {
+    let tmp = this.board;
+    this.board = this.nextBoard;
+    this.nextBoard = tmp;
+  }
 }
 
 /**
@@ -94,13 +187,19 @@ const canvas = document.getElementById("conway-canvas");
 
 if (canvas.getContext) {
   const conway = new GameOfLife(canvas);
-  conway.board.set(0, 0, 1);
-  conway.board.set(1, 0, 1);
-  console.log(conway.board);
-  conway.draw();
 
-  conway.board.set(1, 2, 1);
-  conway.draw();
+  conway.board.set(5, 5, 1);
+  conway.board.set(6, 6, 1);
+  conway.board.set(7, 6, 1);
+  conway.board.set(7, 5, 1);
+  conway.board.set(7, 4, 1);
+
+  setInterval(() => {
+    conway.tick();
+    conway.draw();
+    conway.swapBuffers();
+  }, 100);
+
   // TODO: Handle association with an array
   // TODO: Create class for CGOL
 
